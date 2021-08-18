12315db9fb0b658ade4022b89206f035b8635402
Oscar Sánchez <ojsm45@gmail.com>
2021-08-18 01:07:20 -0300

    Divider component

---
+++ src/shared/Divider/Divider.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { string } from 'prop-types';
+import styles from './Divider.module.css';
+
+const Divider = ({ text }) => (
+  <div className={styles.divider__wrapper}>
+    {text && (
+      <div className={styles.divider_text__wrapper}>
+        <span>{text}</span>
+      </div>
+    )}
+    <div role="separator" className={styles.divider} />
+  </div>
+);
+
+Divider.propTypes = {
+  text: string,
+};
+
+Divider.defaultProps = {
+  text: null,
+};
+
+export default Divider;
